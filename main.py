8c113ecae4db776c7c72d19360344003dfb200d2
Matej <matej.korbar>
2021-03-15 17:10:25 +0100

    Secret number game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import random
 
 from flask import Flask, render_template, request, make_response
 
@@ -42,18 +43,34 @@ def portfolio():
         return render_template('porfolio.html', uname=name)
 
 
-# @app.route('/comment', methods=['POST'])
-# def getData():
-#     print(request.form)
-#     name = request.form.get('commentator-name')
-#     email = request.form.get('commentator-email')
-#     content = request.form.get('comment-content')
-#
-#     print(name)
-#     print(email)
-#     print(content)
-#
-#     return render_template('success.html')
+@app.route("/game", methods=["GET"])
+def game():
+    secret_number = request.cookies.get("secret_number")  # check if there is already a cookie named secret_number
+
+    response = make_response(render_template("game.html"))
+    if not secret_number:  # if not, create a new cookie
+        new_secret = random.randint(1, 30)
+        response.set_cookie("secret_number", str(new_secret))
+
+    return response
+
+
+@app.route("/result", methods=["POST"])
+def result():
+    guess = int(request.form.get("guess"))
+    secret_number = int(request.cookies.get("secret_number"))
+
+    if guess == secret_number:
+        message = "Correct! The secret number is {0}".format(str(secret_number))
+        response = make_response(render_template("result.html", message=message))
+        response.set_cookie("secret_number", str(random.randint(1, 30)))  # set the new secret number
+        return response
+    elif guess > secret_number:
+        message = "Your guess is not correct... try something smaller."
+        return render_template("result.html", message=message)
+    elif guess < secret_number:
+        message = "Your guess is not correct... try something bigger."
+        return render_template("result.html", message=message)
 
 
 if __name__ == '__main__':
