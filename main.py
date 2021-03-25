4752d8e0b02aebdddac7728c3466e25248374d32
Matej <matej.korbar>
2021-03-25 18:47:36 +0100

    Added routes: /profile, /profile/edit, /profile/delete, /users and /user/<id> and all the logic

--- main.py
+++ main.py
@@ -80,5 +80,64 @@ def result():
         return render_template("result.html", finished=False,  message=message)
 
 
+@app.route("/profile", methods=["GET"])
+def profile():
+    token = request.cookies.get('token')
+    user = db.query(User).filter_by(session_token=token).first()
+
+    if user:
+        return render_template("profile.html", user_data=user)
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route("/profile/edit", methods=["GET", "POST"])
+def profile_edit():
+    token = request.cookies.get('token')
+    user = db.query(User).filter_by(session_token=token).first()
+
+    if not user:
+        return redirect(url_for('login'))
+    else:
+        if request.method == "GET":
+            return render_template("profile_edit.html", user_data=user)
+        else:
+            user.email = request.form.get('email')
+            db.add(user)
+            db.commit()
+
+            return redirect(url_for('profile'))
+
+
+# todo: change method to POST
+@app.route("/profile/delete", methods=["GET"])
+def profile_delete():
+    token = request.cookies.get('token')
+    user = db.query(User).filter_by(session_token=token).first()
+
+    if not user:
+        return redirect(url_for('login'))
+    else:
+        db.delete(user)
+        db.commit()
+
+        return render_template("deleted.html")
+
+
+@app.route("/users")
+def all_users():
+    users_list = db.query(User).all()
+
+    return render_template("all_users.html", users=users_list)
+
+
+# beremo vrednost parametra user_id iz URL naslova
+@app.route("/user/<user_id>")
+def user_detail(user_id):
+    user = db.query(User).get(int(user_id))
+
+    return render_template("profile.html", user_data=user)
+
+
 if __name__ == '__main__':
     app.run()
